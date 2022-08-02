e5b810ae939c82910559a8da7f797e60f2d9df81
TarunRajput3201 <tarunrajput3201@gmail.com>
2022-08-03 00:59:49 +0530

    day7

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,10 +6,14 @@ const {createCart,updateCart,getCart,deleteCart}=require("../controllers/cartCon
 const {createOrder,updateOrder}=require("../controllers/orderController")
 const { authentication } = require("../middleware/auth")
 
+//==================================USER API'S========================================================//
+
 router.post("/register",createUser)
 router.post("/login",userLogin)
 router.get("/user/:userId/profile",authentication,getUser)
 router.put("/user/:userId/profile", authentication,Updateprofile)
+
+//==================================PRODUCTS API'S====================================================//
 
 router.post("/products",createProduct)
 router.get("/products",getProduct)
@@ -17,13 +21,19 @@ router.get("/products/:productId",getProductById)
 router.put("/products/:productId",updateProduct)
 router.delete("/products/:productId",deleteProductById)
 
+//==================================CART API'S=========================================================//
+
 router.post("/users/:userId/cart",authentication,createCart)
 router.put("/users/:userId/cart",authentication,updateCart)
 router.get("/users/:userId/cart",authentication,getCart)
 router.delete("/users/:userId/cart",authentication,deleteCart)
 
+//==================================ORDER API'S========================================================//
+
 router.post("/users/:userId/orders",authentication,createOrder)
 router.put("/users/:userId/orders",authentication,updateOrder)
+
+
 
 router.all("/**",function(req,res){
     return res.status(404).send({status:false,message:"No such api found"})
